50284114099c3d51ea366d0ad3ef76de716cae87
SiriKatharina <siri@skeight.de>
2022-01-10 13:21:52 +0100

    Anpassungen wie vorgegeben

--- lib/screens/screen_select_view.dart
+++ lib/screens/screen_select_view.dart
@@ -5,7 +5,11 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:joma/controllers/view_controller.dart';
+import 'package:joma/materials/button.dart';
 import 'package:joma/screens/screen_home.dart';
+
+import 'package:get/get.dart';
+import 'package:joma/materials/assets.dart';
 
 class SelectView extends StatelessWidget {
 
@@ -17,41 +21,51 @@ class SelectView extends StatelessWidget {
       appBar: AppBar(
         title: const Center(
           child: Text(
-            'BEREICH WÄHLEN',
+            'ANSICHT',
             textAlign: TextAlign.center,
-            style: TextStyle(fontFamily: 'Marcellus', fontSize: 20),
+            //style: TextStyle(fontFamily: 'Marcellus', fontSize: 20),
+            style: AppTextStyles.darkH1,
           ),
         ),
-        backgroundColor: const Color.fromRGBO(129, 129, 129, 1),
+        //backgroundColor: const Color.fromRGBO(129, 129, 129, 1),
+          backgroundColor: AppBackgroundColors().darkBackground,
       ),
+      backgroundColor: AppBackgroundColors().darkBackground,
+
       body: Center(
         child: Column(
           children: [
-            //Card1: Nur Bilder
+
+            //Card1: nur Bilder
             GetBuilder<ViewController>(builder: (_) {
               return Flexible(
                 child: GestureDetector(
                   onTap: () {viewController.setView(0);},
                   child: Card(
                     clipBehavior: Clip.antiAlias,
+                    /*child: InkWell(
+                    splashColor: Colors.blue.withAlpha(30),
+                    onTap: () {
+                    debugPrint('Card tapped.');
+                    },*/
                     child: Column(
                       children: [
-                        Flexible(
-                          child: Ink.image(
-                            image: const NetworkImage(
-                                'https://source.unsplash.com/random/800x600?house'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
                         Container(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text("Nur Bilder"),
+
+                         padding: EdgeInsets.all(28.0),
+                         margin: EdgeInsets.all(44.0),
+                          child: Text("Bildsprache",
+                                      style:  AppTextStyles.darkH2,
+                                      textAlign: TextAlign.center),
+
+
                         ),
                       ],
                     ),
                     color: (() {
                       if (_.state == 0) {
-                        return const Color.fromRGBO(184, 219, 255, 1.0);
+                         return AppColors().darkPrimaryColor;
+
                       } else {
                         return Colors.white;
                       }
@@ -81,22 +95,21 @@ class SelectView extends StatelessWidget {
                     clipBehavior: Clip.antiAlias,
                     child: Column(
                       children: [
-                        Flexible(
-                          child: Ink.image(
-                            image: const NetworkImage(
-                                'https://source.unsplash.com/random/800x600?house'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
                         Container(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text("Leichte Sprache"),
+
+                          padding: EdgeInsets.all(28.0),
+                          margin: EdgeInsets.all(44.0),
+                          child: Text("Leichte Sprache",
+                              style:  AppTextStyles.darkH2,
+                              textAlign: TextAlign.center),
+
+
                         ),
                       ],
                     ),
                     color: (() {
                       if (viewController.state == 1) {
-                        return const Color.fromRGBO(184, 219, 255, 1.0);
+                        return AppColors().darkPrimaryColor;
                       } else {
                         return Colors.white;
                       }
@@ -126,22 +139,21 @@ class SelectView extends StatelessWidget {
                     clipBehavior: Clip.antiAlias,
                     child: Column(
                       children: [
-                        Flexible(
-                          child: Ink.image(
-                            image: const NetworkImage(
-                                'https://source.unsplash.com/random/800x600?house'),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
                         Container(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text("Volltext"),
+
+                          padding: EdgeInsets.all(28.0),
+                          margin: EdgeInsets.all(44.0),
+                          child: Text("Volltext",
+                              style:  AppTextStyles.darkH2,
+                              textAlign: TextAlign.center),
+
+
                         ),
                       ],
                     ),
                     color: (() {
                       if (_.state == 2) {
-                        return const Color.fromRGBO(184, 219, 255, 1.0);
+                        return AppColors().darkPrimaryColor;
                       } else {
                         return Colors.white;
                       }
@@ -167,25 +179,14 @@ class SelectView extends StatelessWidget {
       ),
       bottomNavigationBar: Container(
         height: 60,
-        color: Colors.black12,
-        child: InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomeScreen()),
-            );
-          },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                "Weiter",
-                style: TextStyle(fontFamily: 'Marcellus', fontSize: 20),
-              ),
-            ],
-          ),
+        //color: Colors.black12,
+        color: AppBackgroundColors().darkBackground,
+        child:
+        AppButton(text: "Weiter",
+                  color: AppColors().darkSecondaryColor,
+                  onPressed:(){ Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));},
         ),
+
       ),
     );
   }
