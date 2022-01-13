2e635f901132caab2ac8c39a3832beebe7243661
Adrian Somesan <som.adrian@gmail.com>
2022-01-13 15:08:23 +0100

    changed profile loading from future builder to data controller.

--- lib/screens/screen_profil_settings_loader.dart
+++ lib/screens/screen_profil_settings_loader.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:joma/controllers/data_controller.dart';
 import 'package:joma/global/glovar.dart';
 import 'package:joma/materials/assets.dart';
 import 'package:joma/model/profil_model.dart';
@@ -22,6 +24,7 @@ class ProfilSettingsLoader extends StatefulWidget {
 
 
 class _ProfilSettingsLoaderState extends State<ProfilSettingsLoader> {
+  final DataController data = Get.find();
 
   @override
   void initState() {
@@ -29,11 +32,390 @@ class _ProfilSettingsLoaderState extends State<ProfilSettingsLoader> {
     super.initState();
   }
 
+  final NameController = TextEditingController();
+  final EmailController = TextEditingController();
+  final TelController = TextEditingController();
+  final AdressController = TextEditingController();
+  final StadtController = TextEditingController();
+
+  bool nameCheck = false;
+  bool emailCheck = false;
+  bool telCheck = false;
+  bool adressCheck = false;
+  bool stadtCheck = false;
+
 
 
   @override
   Widget build(BuildContext context) {
+    Profil user = data.profile[2];
+    var name = user.vorname! + " " + user.name!;
+    var stadt = user.adresse!.plz! +  " " + user.adresse!.ort!;
+    var email = user.kontakt!.email;
+    var tel = user.kontakt!.tel;
+    var adresse = user.adresse!.strasse! + " " + user.adresse!.hausnummer!;
+    var bild = user.bild;
     return Scaffold(
+      appBar: AppBar(
+          title: Text("Profil bearbeiten"),
+          centerTitle: true,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Get.off(() => ProfilLoader());
+            },
+          ),
+          backgroundColor: Glovar.white,
+          foregroundColor: Glovar.blackvar,
+          actions: [
+            IconButton(
+                icon: Icon(Icons.settings),
+                onPressed: () {
+                  // Button linking to settings page
+                }),
+          ]),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Center(
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  margin: EdgeInsets.only(top: 25.0),
+                  decoration: BoxDecoration(
+                    //shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage(
+                          bild!),
+                      fit: BoxFit.cover,
+                    ),
+                    border: Border.all(
+                      color: Glovar.grey,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Container(
+                  width: 120,
+                  height: 35,
+                  margin: EdgeInsets.only(top: 10, bottom: 30),
+                  child: TextButton(
+                    child: Text('Avatar ändern'),
+                    onPressed: () {
+                      // Button to change the profile picture
+                    },
+                    style: TextButton.styleFrom(
+                      primary: Glovar.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Glovar.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20),
+              child: Column(
+                children: [
+                  Container(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Name',
+                        style: TextStyle(color: Glovar.blackvar),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(right: 20),
+                      child: TextFormField(
+                          controller: NameController,
+                          onChanged: (text) {
+                            nameCheck = true;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: name,
+                          )
+                      )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'E-Mail',
+                        style: TextStyle(color: Glovar.blackvar),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(right: 20),
+                      child: TextFormField(
+                          controller: EmailController,
+                          onChanged: (text) {
+                            emailCheck = true;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: email,
+                          )
+                      )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Telefon',
+                        style: TextStyle(color: Glovar.blackvar),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(right: 20),
+                      child: TextFormField(
+                          controller: TelController,
+                          onChanged: (text) {
+                            telCheck = true;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: tel,
+                          )
+                      )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Adresse',
+                        style: TextStyle(color: Glovar.blackvar),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(right: 20),
+                      child: TextFormField(
+                          controller: AdressController,
+                          onChanged: (text) {
+                            adressCheck = true;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: adresse,
+                          )
+                      )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Stadt',
+                        style: TextStyle(color: Glovar.blackvar),
+                      ),
+                    ),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(right: 20),
+                      child: TextFormField(
+                          controller: StadtController,
+                          onChanged: (text) {
+                            stadtCheck = true;
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: stadt,
+                          )
+                      )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Stärken',
+                        style: TextStyle(color: Glovar.blackvar),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        children: [
+                          Container(
+                              child: InputChip(
+                                  label: const Text('Belastbarkeit'),
+                                  labelStyle: TextStyle(color: Glovar.white),
+                                  backgroundColor: Glovar.red,
+                                  onPressed: () {
+                                    print('I am the one thing in life.');
+                                  })),
+                          Container(
+                              margin: EdgeInsets.only(left: 5),
+                              child: InputChip(
+                                  label: const Text('Teamfähigkeit'),
+                                  labelStyle: TextStyle(color: Glovar.white),
+                                  backgroundColor: Glovar.blue,
+                                  onPressed: () {
+                                    print('I am the one thing in life.');
+                                  })),
+                          Container(
+                              margin: EdgeInsets.only(left: 5),
+                              child: InputChip(
+                                  label: const Text('Offenheit'),
+                                  labelStyle: TextStyle(color: Glovar.white),
+                                  backgroundColor: Glovar.green,
+                                  onPressed: () {
+                                    print('I am the one thing in life.');
+                                  })),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                        children: [
+                          Container(
+                              child: InputChip(
+                                  label: const Text('Engagement'),
+                                  labelStyle: TextStyle(color: Glovar.white),
+                                  backgroundColor: Glovar.purple,
+                                  onPressed: () {
+                                    print('I am the one thing in life.');
+                                  })),
+                          Container(
+                              margin: EdgeInsets.only(left: 5),
+                              child: InputChip(
+                                  label: const Text('Geduld'),
+                                  labelStyle: TextStyle(color: Glovar.white),
+                                  backgroundColor: Glovar.orange,
+                                  onPressed: () {
+                                    print('I am the one thing in life.');
+                                  })),
+                          Container(
+                              margin: EdgeInsets.only(left: 5),
+                              child: IconButton(
+                                  icon: Icon(Icons.add),
+                                  onPressed: () {
+                                    // Button linking to settings page
+                                  })
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Center(
+                child: Container(
+                  width: 140,
+                  height: 35,
+                  margin: EdgeInsets.only(top: 35, bottom: 50),
+                  child: TextButton(
+                    child: Text('Speichern'),
+                    onPressed: () async {
+                      if (emailCheck) {
+                        await UserSimplePreferences.setEmail(
+                            EmailController.text);
+                      }
+
+                      if (nameCheck) {
+                        await UserSimplePreferences.setName(
+                            NameController.text);
+                      }
+
+                      if (telCheck) {
+                        await UserSimplePreferences.setTelefon(
+                            TelController.text);
+                      }
+
+                      if (adressCheck) {
+                        await UserSimplePreferences.setAdresse(
+                            AdressController.text);
+                      }
+
+                      if (stadtCheck) {
+                        await UserSimplePreferences.setStadt(
+                            StadtController.text);
+                      }
+                    },
+                    style: TextButton.styleFrom(
+                      primary: Glovar.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: Glovar.grey,
+
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: Container(
+        height: 80.0,
+        width: 80.0,
+        child: FloatingActionButton(
+          elevation: 0,
+          child: CircleAvatar(
+            radius: 80.0,
+            backgroundImage: AssetImage('assets/images/darkJomaLogo.png',
+            ),
+          ),
+          onPressed: () {
+            Get.off(() => ScreenHome());
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 1,
+          onTap: (value) {
+            if (value == 0) Get.off(() => JobListSearchScreen());
+            if (value == 1) Get.off(() => ProfilLoader());
+            //);
+            //if (value == 2) Navigator.of(context).push(...);
+          },
+          backgroundColor: AppColors().darkPrimaryColor,
+          selectedItemColor: AppColors().white,
+          unselectedItemColor: AppColors().white,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: AppIcons().searchGlass,
+              label: 'Suchen',
+            ),
+            BottomNavigationBarItem(
+              icon: AppIcons().profile,
+              label: 'Profil',
+            ),
+          ]
+      ),
+      floatingActionButtonLocation:
+      FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+}
+    //old profile function used with future builder
+    /*
       body: FutureBuilder<List<Profil>>(
           future: RemoteServices.fetchProfil(),
 
@@ -54,6 +436,7 @@ class _ProfilSettingsLoaderState extends State<ProfilSettingsLoader> {
     );
   }
 }
+
 
 
 
@@ -384,6 +767,7 @@ Widget profile(List<Profil>? profil) {
                     }
 
 
+
                   },
                   style: TextButton.styleFrom(
                     primary: Glovar.white,
@@ -442,3 +826,4 @@ Widget profile(List<Profil>? profil) {
     FloatingActionButtonLocation.centerDocked,
   );
 }
+*/
