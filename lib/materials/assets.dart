fa91a0aad8d4147c3baff55bea75780586a8bff2
natoriouskpk <TristanGrobasch@googlemail.com>
2022-01-17 12:26:42 +0100

    - removed previous added textstyle

--- lib/materials/assets.dart
+++ lib/materials/assets.dart
@@ -39,8 +39,6 @@ zum Einbinden:
 class AppTextStyles {
   static const darkH1 = TextStyle(
       fontFamily: AppFont.mainFont, fontSize: 25, fontWeight: FontWeight.bold);
-  static const darkH1black = TextStyle(
-      fontFamily: AppFont.mainFont, fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black);
   static const darkH2 = TextStyle(
       fontFamily: AppFont.mainFont, fontSize: 23, fontWeight: FontWeight.bold);
   static const darkH3 = TextStyle(
