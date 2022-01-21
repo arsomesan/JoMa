e0da9d25e78456649df2da6f6be37ab8d41ecd21
TeddyCreme <tpunkt.photography@gmail.com>
2022-01-21 17:34:17 +0100

    job category

--- lib/screens/screen_test.dart
+++ lib/screens/screen_test.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:joma/materials/appbar.dart';
+import 'package:joma/materials/appbar_job.dart';
+import 'package:joma/materials/assets.dart';
 
 class SecondRoute extends StatelessWidget {
   const SecondRoute({Key? key}) : super(key: key);
@@ -7,17 +10,28 @@ class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Second Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
+        backgroundColor: AppBackgroundColors().darkBackground,
+        body: Center(
+            child: ListView(children: <Widget>[
+          Container(
+            height: 255,
+            margin: const EdgeInsets.only(bottom: 280),
+            child: AppBarJobArea(
+              bgColor: AppBackgroundColors().darkBackground,
+              bgColorBar: AppColors().darkPrimaryColor,
+              color: AppColors().darkPrimaryColor,
+              title: 'Willkommen \nbei JOMA'.toUpperCase(),
+              onPressed1: () {},
+              onPressed2: () {},
+            ),
+
+            // child: ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pop(context);
+            //   },
+            //   child: const Text('Go back!'),
+            // ),
+          ),
+        ])));
   }
 }
